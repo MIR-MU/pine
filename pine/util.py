baf05b822293f68279a4df3c3450a19421fe8edc
Vít Novotný <witiko@mail.muni.cz>
2021-01-25 02:29:16 +0100

    Make util.download_to check that the download did not fail

--- pine/util.py
+++ pine/util.py
@@ -9,8 +9,12 @@ from tqdm import tqdm
 
 def download_to(url: str, size: int, path: Path, buffer_size: int = 2**20):
     desc = 'Downloading {} to {}'.format(url, path)
+    downloaded = 0
     with tqdm(total=size, unit='B', desc=desc) as pbar:
         with path.open('wb') as wf, smart_open.open(url, 'rb') as rf:
             for data in iter(partial(rf.read, buffer_size), b''):
                 wf.write(data)
+                downloaded += len(data)
                 pbar.update(len(data))
+    if size != downloaded:
+        raise ValueError('Downloaded {} bytes, expected {} bytes'.format(downloaded, size))
